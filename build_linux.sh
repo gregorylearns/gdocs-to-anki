155f12acaae209dc059253aee1e575757bc4eb10
Greg <degeemon@gmail.com>
2025-05-26 06:16:25 +0800

    add windows cmd command

--- build_linux.sh
+++ build_linux.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
 pyinstaller --clean --windowed --onefile --icon ico/heartonfireopenmoji.png --upx-dir=upx/upx-5.0.0-amd64_linux/ gui_qt6.py
+
+
+
+# Windows build
+# pyinstaller --clean --windowed --onefile --icon ico/heartonfireopenmoji.png --upx-dir=upx/upx-5.0.1-win64/ gui_qt6.py
